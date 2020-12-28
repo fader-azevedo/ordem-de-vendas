0de0df740d0ab248ea025a793af146673a4d7ee3
Alcino da Graca <alcinodagrsca@gmail.com>
2020-12-28 15:50:11 +0200

    primeiro commit

---
+++ application/views/home/index.php
@@ -0,0 +1,20 @@
+
+<?php $this->load->view('layout/sidebar') ?>
+
+<?php $this->load->view('layout/navbar') ?>
+
+
+	<!-- Main Content -->
+	<div id="content">
+
+		<!-- Begin Page Content -->
+		<div class="container-fluid">
+
+			<!-- Page Heading -->
+			<h1 class="h3 mb-4 text-gray-800">Home</h1>
+
+		</div>
+		<!-- /.container-fluid -->
+
+	</div>
+	<!-- End of Main Content -->
